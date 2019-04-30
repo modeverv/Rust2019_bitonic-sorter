9e18d89eedf55cf0bee870f155be32c3ec000820
modeverv <modeverv@gmail.com>
2019-05-01 02:59:16 +0900

    initial

---
+++ src/first.rs
@@ -0,0 +1,17 @@
+
+#[allow(non_snake_case)]
+pub fn sort(x: &mut [u32], up: bool) {
+    unimplemented!()
+    if x.len() > 1 {
+        let mid_point = x.len() / 2
+    }
+}
+
+#[allow(non_upper_case_globals)]
+fn sub_sort(x: &mut [u32], up: bool){
+    unimplemented!()
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!()
+}
